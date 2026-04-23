ffab02fe04cc0d28a751728c5149e4ecbb8000cd
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-04-23 10:31:47 +0200

    feat(deps)

---
+++ salaries/src/salaries/app.py
@@ -0,0 +1,2 @@
+# Streamlit app
+import streamlit as st
---
+++ salaries/src/salaries/utils/constants.py
@@ -0,0 +1,12 @@
+# Konstants, rör ej.
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parents[1]
+ASSETS_PATH = BASE_PATH / "assets"
+
+
+COMPONENTS_PATH = ASSETS_PATH / "components"
+IMAGE_PATH = ASSETS_PATH / "image"
+STYLES_PATH = ASSETS_PATH / "style"
+MARKDOWN_PATH = ASSETS_PATH / "markdown"
+DATA_PATH = ASSETS_PATH / "data"
